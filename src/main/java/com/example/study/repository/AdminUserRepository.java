9ab11f500a567ff55b32715b80f8af17d8a16402
KangDaeBeom <rkdeoqja@nate.com>
2021-03-06 00:14:23 +0900

    Spring adminPage study 21-03-06 00:14am

--- src/main/java/com/example/study/repository/AdminUserRepository.java
+++ src/main/java/com/example/study/repository/AdminUserRepository.java
@@ -1,13 +1,11 @@
 package com.example.study.repository;
 
-import com.example.study.model.entity.User;
+import com.example.study.model.entity.AdminUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface AdminUserRepository extends JpaRepository<AdminUser,Long> {
+
 
 }
-
