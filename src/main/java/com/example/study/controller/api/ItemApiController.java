11361e66049020256634e29ca37bbadc63842b71
KangDaeBeom <rkdeoqja@nate.com>
2021-03-12 23:54:38 +0900

    Spring adminPage study 21-03-12 11:54pm

---
+++ src/main/java/com/example/study/controller/api/ItemApiController.java
@@ -0,0 +1,40 @@
+package com.example.study.controller.api;
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.ItemApiRequest;
+import com.example.study.model.network.response.ItemApiResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/item")
+
+public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+    @Override
+    @PostMapping("")    // /api/item
+    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    @GetMapping("{id}") // /api/item/1....1000
+    public Header<ItemApiResponse> read(@PathVariable Long id) {
+        return null;
+    }
+
+    @Override
+    @PutMapping("")// /api/item
+    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    @DeleteMapping("{id}") // /api/item/1....1000
+    public Header<ItemApiResponse> delete(@PathVariable Long id) {
+        return null;
+    }
+}
