9f009f7f2a4c56b72c2bab6098db5ab5a58ffd29
KangDaeBeom <rkdeoqja@nate.com>
2021-03-15 23:19:37 +0900

    Spring adminPage study 21-03-15 11:19pm

---
+++ src/main/java/com/example/study/service/ItemApiLogicService.java
@@ -0,0 +1,55 @@
+package com.example.study.service;
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.entity.Item;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.ItemApiRequest;
+import com.example.study.model.network.response.ItemApiResponse;
+import com.example.study.repository.PartnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class ItemApiLogicService implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+
+    @Autowired
+    private PartnerRepository partnerRepository;
+
+    @Override
+    public Header<ItemApiResponse> create(Header<ItemApiRequest> request) {
+
+        ItemApiRequest body = request.getData();
+
+        Item item = Item.builder()
+                .status(body.getStatus())
+                .name(body.getName())
+                .title(body.getTitle())
+                .content(body.getContent())
+                .price(body.getPrice())
+                .brandName(body.getBrandName())
+                .registeredAt(LocalDateTime.now())
+                .partner(partnerRepository.getOne(body.getPartnerId()))
+                .build();
+
+
+        return null;
+    }
+
+    @Override
+    public Header<ItemApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header<ItemApiResponse> update(Header<ItemApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header<ItemApiResponse> delete(Long id) {
+        return null;
+    }
+}
