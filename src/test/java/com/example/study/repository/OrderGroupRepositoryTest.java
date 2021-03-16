cfe9451d7f96c208e93cffc2b5e2acb51f0f8d97
KangDaeBeom <rkdeoqja@nate.com>
2021-03-17 03:18:54 +0900

    Spring adminPage study 21-03-17 03:17am

--- src/test/java/com/example/study/repository/OrderGroupRepositoryTest.java
+++ src/test/java/com/example/study/repository/OrderGroupRepositoryTest.java
@@ -1,13 +1,21 @@
 package com.example.study.repository;
 
 import com.example.study.StudyApplicationTests;
+import com.example.study.component.LoginUserAuditorAware;
+import com.example.study.config.JpaConfig;
 import com.example.study.model.entity.OrderGroup;
+import com.example.study.model.entity.User;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class OrderGroupRepositoryTest extends StudyApplicationTests {
 
@@ -21,7 +29,7 @@ public class OrderGroupRepositoryTest extends StudyApplicationTests {
         orderGroup.setStatus("COMPLETE");
         orderGroup.setOrderType("ALL");
         orderGroup.setRevAddress("서울시 강남구");
-        orderGroup.setRevName("홍길동");
+        orderGroup.setRevName("강낭콩");
         orderGroup.setPaymentType("CARD");
         orderGroup.setTotalPrice(BigDecimal.valueOf(900000));
         orderGroup.setTotalQuantity(1);
@@ -29,7 +37,8 @@ public class OrderGroupRepositoryTest extends StudyApplicationTests {
         orderGroup.setArrivalDate(LocalDateTime.now());
         orderGroup.setCreatedAt(LocalDateTime.now());
         orderGroup.setCreatedBy("AdminServer");
-      //  orderGroup.setUserId(1L);
+
+        //orderGroup.setUser();
 
         OrderGroup newOrderGroup = orderGroupRepository.save(orderGroup);
         Assertions.assertNotNull(newOrderGroup);
